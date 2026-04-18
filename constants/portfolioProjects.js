616409a7a7c7650f425bc58f21625ed643c1dc76
Tayyab Faisal <codertayyab101@gmail.com>
2026-04-19 02:20:52 +0500

    remove other projects from project portfolio

--- constants/portfolioProjects.js
+++ constants/portfolioProjects.js
@@ -1,43 +1,5 @@
 export const projectsData = [
-  {
-    name: "Operon",
-    year: "2026",
-    slug: "operon",
-    imageDesktop: "/mockups/operon/laptop.png",
-    imageMobile: "/mockups/operon/laptop.png"
-  },
 
-  {
-    name: "AutonomyOps AI",
-    year: "2026",
-    slug: "autonomyops-ai",
-    imageDesktop: "/mockups/autonomyops-ai/mockup.png",
-    imageMobile: "/mockups/autonomyops-ai/mockup.png"
-  },
-
-  {
-    name: "FactoryVision",
-    year: "2026",
-    slug: "factoryvision",
-    imageDesktop: "/mockups/factoryvision/laptop.jpeg",
-    imageMobile: "/mockups/factoryvision/laptop.jpeg"
-  },
-
-{
-  name: "3D Garage Configurator",
-  year: "2026",
-  slug: "garage-configurator",
-  imageDesktop: "/mockups/garage-configurator/laptop.png",
-  imageMobile: "/mockups/garage-configurator/laptop.png"
-},
-
-{
-  name: "3D Sofa Configurator",
-  year: "2026",
-  slug: "sofa-3d-configurator",
-  imageDesktop: "/mockups/sofa-3d-configurator/mockup.png",
-  imageMobile: "/mockups/sofa-3d-configurator/mockup.png"
-},
 
   {
     name: "Neighbourly Marketplace",
@@ -54,54 +16,11 @@ export const projectsData = [
     imageMobile: "/mockups/orion-workspace/laptop.png",
   },
   {
-    name: "Skill AI Scan",
-    year: "2026",
-    slug: "skill-ai-scan",
-    imageDesktop: "/mockups/skill-ai-scan/laptop.png",
-    imageMobile: "/mockups/skill-ai-scan/laptop.png",
-  },
-  {
     name: "AsliAIMonitor",
     year: "2026",
     slug: "asli-ai-monitor",
     imageDesktop: "/mockups/asli-ai-monitor/laptop.png",
     imageMobile: "/mockups/asli-ai-monitor/laptop.png",
-  },
-
-  {
-    name: "E-Commerce",
-    year: "2025",
-    slug: "exclusive-ecom",
-    imageDesktop: "/mockups/ecom/Ipad.webp",
-    imageMobile: "/mockups/ecom/iPhone.webp",
-  },
-  {
-    name: "Real-Estate",
-    year: "2025",
-    slug: "zarghab-real-estate",
-    imageDesktop: "/mockups/real-estate/Ipad.webp",
-    imageMobile: "/mockups/real-estate/iPhone.webp",
-  },
-  {
-    name: "EZY Logistics",
-    year: "2025",
-    slug: "ezy-logistics",
-    imageDesktop: "/mockups/ezzy/Ipad.webp",
-    imageMobile: "/mockups/ezzy/iPhone.webp",
-  },
-  {
-    name: "3D Book",
-    year: "2025",
-    slug: "book-slider",
-    imageDesktop: "/mockups/3D-book/Ipad.webp",
-    imageMobile: "/mockups/3D-book/iPhone.webp",
-  },
-  {
-    name: "Soch Ai",
-    year: "2025",
-    slug: "soch-ai-chatbot",
-    imageDesktop: "/mockups/soch/Ipad.webp",
-    imageMobile: "/mockups/soch/iPhone.webp",
   },
 
 ];
